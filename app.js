f6fafcd30d08a391e9dea6dbb0aef74e682362ab
Noor Alam <161155753+nooralam12345@users.noreply.github.com>
2025-06-12 19:40:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+ const close=document.querySelector(".close"); 
+ const open=document.querySelector(".ham");  
+ constmenu=document.querySelector(".menu")  
+ close.addEventListener("click",()=>{ 
+menu.style.visibility="hidden";  }) 
+open.addEventListener("click",()=>{ 
+menu.style.visibility="visible";  })
